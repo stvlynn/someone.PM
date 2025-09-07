9eb39b96d6397c93b2e32716993804de970ad8ae
Steven Lynn <i@stv.lol>
2025-09-08 00:49:42 +0800

    chore: remove unused React import from RejectedByPage component

--- src/components/pages/RejectedByPage.tsx
+++ src/components/pages/RejectedByPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import LogoLoop from '@/components/LogoLoop'
 import './RejectedByPage.css'
 import yaml from 'js-yaml'
